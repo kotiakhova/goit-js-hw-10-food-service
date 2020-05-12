e51a9d23534fab95c7f635125c35ade133372cce
Евгения <macbook@MacBook-Air.local>
2020-05-12 13:48:03 +0300

    homework-10-1

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import posts from '../menu.json';
+import postsItemTemplate from './templates/menu-item.hbs';
+import './js/theme_changer'
+import './styles.css';
+
+
+
+const menuList = document.querySelector('ul.js-menu');
+console.log(menuList);
+
+
+
+function buildPost(posts) {
+    const markup = posts.map( post => postsItemTemplate(post)).join('')
+    return markup
+
+    
+
+
+}
+menuList.insertAdjacentHTML('beforeend', buildPost(posts));
---
+++ src/js/theme_changer.js
@@ -0,0 +1,50 @@
+const input = document.querySelector('.js-switch-input');
+
+const body = document.querySelector('body');
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+const currentTheme = localStorage.getItem('theme');
+
+
+localStorage.setItem('theme', 'LIGHT');
+
+handleLocalStorageTheme();
+
+input.addEventListener('change', handleClick);
+
+function handleLocalStorageTheme (){
+    if(currentTheme){
+        if(currentTheme === 'LIGHT') {
+            input.checked = false;
+            body.classList.add(Theme.LIGHT);
+            body.classList.remove(Theme.DARK);
+
+        } else {
+            body.classList.add(Theme.DARK);
+            body.classList.remove(Theme.LIGHT);
+            input.checked = true;
+        }
+      }
+}
+
+
+function handleClick() {
+    if(input.checked) {
+        localStorage.setItem('theme', 'DARK');
+        body.classList.remove(Theme.LIGHT);
+        body.classList.add(Theme.DARK);
+        input.checked = true;
+    } else {
+        body.classList.add(Theme.LIGHT);
+        body.classList.remove(Theme.DARK);
+        localStorage.setItem('theme', 'LIGHT');
+        input.checked = false;
+        }
+
+    }
+
+;
